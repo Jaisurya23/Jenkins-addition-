483daf77277a942db99cea8f059b5d9391aa9170
Jaisurya23 <jaisurya230904@gmail.com>
2025-08-01 12:06:49 +0530

    I added the jenkins files

--- add.py
+++ add.py
@@ -2,6 +2,4 @@ def add(a,b):
     result = a + b
     return f"The sum of {a} and {b} is {result}."
 if __name__ == "__main__":   
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: ")) 
-    print(add(a,b))                   
+    print(add(2,3))                   
