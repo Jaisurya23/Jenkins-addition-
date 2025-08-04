b34b97300c785afa0f72b071ba8925b0f9ab24b5
Jaisurya23 <jaisurya230904@gmail.com>
2025-08-04 16:13:48 +0530

    Add __init__.py file for test

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def add(a,b):
-    result = a + b
-    return f"The sum of {a} and {b} is {result}."
-if __name__ == "__main__":   
-    print(add(2,3))                   
+    c = a + b
+    return f"The sum of {a} and {b} is {c}."
+if __name__ == "__main__":
+    print(add(2, 3))  # Example usage
